917048e3f6e3bbc5a32993db5f437ecc76e60759
Nick de Rozarieux <banterstudios@gmail.com>
2017-10-04 21:58:26 +0100

    removed redundant comments

--- client/index.js
+++ client/index.js
@@ -1,41 +1,11 @@
-/**
- * @module styles
- */
 import 'client/styles/index.scss'
 
-/**
- * @module react
- */
-import React from 'react'
-
-/**
- * @module react-dom
- */
 import { render } from 'react-dom'
 
-/**
- * @module react-redux
- */
-// import { Provider } from 'react-redux'
-
-/**
- * @module router
- */
 import router from 'client/config/router'
 
-/**
- * @module store
- */
-// import store from './store'
+import { queryById } from 'client/utils/domUtils'
 
-/**
- *  queryById
- */
-import { queryById } from 'client/utils/DomUtils'
-
-/**
- * @module injectTapEventPlugin
- */
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
